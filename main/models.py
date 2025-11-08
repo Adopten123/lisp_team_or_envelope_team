57858db261a8e8e6769efc7204116b51eaa70215
Alexander Mikhailov <alexandermikhailov0505@gmail.com>
2025-11-08 16:45:32 +0300

    Создание модели Программы обучения

--- main/models.py
+++ main/models.py
@@ -10,9 +10,11 @@ class Person(models.Model):
         settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True, blank=True,
         related_name='person', help_text="Связь с Django User (если есть)"
     )
+
     last_name = models.CharField("Фамилия", max_length=128)
     first_name = models.CharField("Имя", max_length=128)
     middle_name = models.CharField("Отчество", max_length=128, blank=True)
+
     email = models.EmailField(blank=True)
     phone = models.CharField(max_length=32, blank=True)
     vk_user_id = models.CharField(
@@ -32,3 +34,17 @@ class Faculty(models.Model):
     def __str__(self):
         return self.name
 
+class Program(models.Model):
+    """
+    Программа обучения (Программная Инженерия и прочее)
+    """
+    faculty = models.ForeignKey(Faculty, on_delete=models.PROTECT, related_name='programs')
+    name = models.CharField(max_length=255) # название направления
+    code = models.CharField(max_length=32, blank=True) # код направления
+    duration_years = models.PositiveSmallIntegerField(default=4) # длительность обучения в годах
+
+    class Meta:
+        unique_together = [("faculty", "name")]
+
+    def __str__(self):
+        return f"{self.code or ''} {self.name}".strip()
