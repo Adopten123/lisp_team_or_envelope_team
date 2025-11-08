cedfce31d9725cca322a0f2784177b188456f54b
Alexander Mikhailov <alexandermikhailov0505@gmail.com>
2025-11-08 17:59:37 +0300

    Создание объекта учебный план

--- main/models.py
+++ main/models.py
@@ -114,3 +114,24 @@ class Discipline(models.Model):
 
     def __str__(self):
         return self.title
+
+class Curriculum(models.Model):
+    """
+    Класс учебного плана.
+    На какой программе и в каком семестре читается дисциплина,
+    рекомендуемые часы/контроль и т.п.
+    """
+    SEMESTER_CHOICES = [(i, f"{i}") for i in range(1, 13)]
+
+    program = models.ForeignKey(Program, on_delete=models.CASCADE, related_name='curriculum') # название учебной программы
+    discipline = models.ForeignKey(Discipline, on_delete=models.PROTECT, related_name='curriculum_items') #дисциплна
+    semester = models.PositiveSmallIntegerField(choices=SEMESTER_CHOICES) #семестр, в котором изучается программа
+    hours = models.PositiveSmallIntegerField(default=0) # количество часов
+    control_form = models.CharField(
+        max_length=32,
+        choices=[("exam", "Экзамен"), ("test", "Зачет"), ("diff_test", "Дифф. зачет")],
+        default="exam"
+    ) # форма оценивания
+
+    class Meta:
+        unique_together = [("program", "discipline", "semester")]
