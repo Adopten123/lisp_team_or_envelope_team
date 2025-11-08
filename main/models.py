14b7df688d4d9470039e3430c7460b3989da2613
Alexander Mikhailov <alexandermikhailov0505@gmail.com>
2025-11-08 16:22:27 +0300

    Создание модели Person

--- main/models.py
+++ main/models.py
@@ -1,3 +1,26 @@
+from django.conf import settings
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 
-# Create your models here.
+class Person(models.Model):
+    """
+    Базовый объект человека.
+    """
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True, blank=True,
+        related_name='person', help_text="Связь с Django User (если есть)"
+    )
+    last_name = models.CharField("Фамилия", max_length=128)
+    first_name = models.CharField("Имя", max_length=128)
+    middle_name = models.CharField("Отчество", max_length=128, blank=True)
+    email = models.EmailField(blank=True)
+    phone = models.CharField(max_length=32, blank=True)
+    vk_user_id = models.CharField(
+        "VK/Max user id", max_length=64, blank=True, db_index=True
+    )
+
+    class Meta:
+        indexes = [models.Index(fields=["vk_user_id"])]
+
+    def __str__(self):
+        return f"{self.last_name} {self.first_name}"
