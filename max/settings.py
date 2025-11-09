58801143c0766ad55543ab9f6c99dbe10d6c273c
ksen0v <frostdog080@gmail.com>
2025-11-09 18:09:06 +0300

    Добавление временных значений allowed_hosts для разработки

--- max/settings.py
+++ max/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure--19m2ln=imsl)s8w=hd%s(g_*b3^a(i84ib0f-8hr&ya@2$b@%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['188.225.82.60', 'localhost']
 
 
 # Application definition
