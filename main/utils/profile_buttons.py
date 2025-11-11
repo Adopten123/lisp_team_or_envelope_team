d4a47eeb2ffbde5e0e751f09362a0188db3b70bb
dontbel1eveme <vanyapopkov05@gmail.com>
2025-11-11 20:15:00 +0300

    profile_buttons.py

---
+++ main/utils/profile_buttons.py
@@ -0,0 +1,12 @@
+profile_buttons =[
+    {'id': 'schedule', 'icon': '📅', 'text': 'Расписание'},
+    {'id': 'grades', 'icon': '📊', 'text': 'Оценки'},
+    {'id': 'studentRequest', 'icon': '📝', 'text': 'Заказать справку'},
+    {'id': 'courses', 'icon': '👥', 'text': 'Моя группа'},
+    {'id': 'schedule', 'icon': '📅', 'text': 'Расписание'},
+    {'id': 'journal', 'icon': '📖', 'text': 'Журнал'},
+    {'id': 'materials', 'icon': '📝', 'text': 'Материалы'},
+    {'id': 'students', 'icon': '👥', 'text': 'Студенты'},
+    {'id': 'application', 'icon': '📄', 'text': 'Подать заявление'},
+    {'id': 'info', 'icon': '🏫', 'text': 'О вузе'},
+]
