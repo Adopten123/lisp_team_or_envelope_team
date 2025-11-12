b6f0c2b31c1684a947eea2e4ab4a3f30eaaaab87
Alexander Mikhailov <alexandermikhailov0505@gmail.com>
2025-11-12 10:39:34 +0300

    Восстановление journalist.py

--- main/views/journalist.py
+++ main/views/journalist.py
@@ -2,9 +2,31 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from ..models import NewsPost, Person, StudentRole
 
+
 def journalist_news_view(request):
     """
     Функция для добавления новостей.
     Добавление новостей доступно для журналиста.
     """
-    pass
+    try:
+        PAGINATOR_COUNT = 10
+
+        person = Person.objects.get(user=request.user)
+        posts = NewsPost.objects.filter(
+            author=person
+        )
+
+        posts_count = NewsPost.objects.filter(
+            author=person.user,
+            is_published=True
+        ).count()
+
+        context = {
+            'person': person,
+            'posts_count': posts_count
+        }
+        return render(request, 'main/journalist/news_create.html', context)
+
+    except Person.DoesNotExist:
+        pass
+
