3a52b00887cf0b247a9916e30ba43c805bb8178e
ksen0v <frostdog080@gmail.com>
2025-11-13 18:02:05 +0300

    Добавление пути для обработки исключения неподдерживаемой платформы

--- authentication/urls.py
+++ authentication/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('api/max-auth/', views.max_auth_view, name='max_auth'),
+    path('unsupported-platform/', views.unsupported_platform_view, name='unsupported_platform')
 ]
